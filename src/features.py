cd7c0fe3b76e52f267ba5ad40765d07ccbe7f089
Bishal Nengminja <bishalnengminja99@gmail.com>
2025-11-08 00:41:23 +0600

    feat: Implement features logic for churn prediction

--- src/features.py
+++ src/features.py
@@ -4,16 +4,26 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 
 def build_transformer(numeric_features, categorical_features):
+    # Use sparse_output if available
+    if "sparse_output" in OneHotEncoder.__init__.__code__.co_varnames:
+        onehot = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+    else:
+        onehot = OneHotEncoder(handle_unknown="ignore", sparse=False)
+
     numeric_transformer = Pipeline([
         ("imputer", SimpleImputer(strategy="median")),
         ("scaler", StandardScaler())
     ])
+    
     categorical_transformer = Pipeline([
-        ("imputer", SimpleImputer(strategy="constant", fill_value="unknown")),
-        ("onehot", OneHotEncoder(handle_unknown="ignore", sparse=False))
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", onehot)
     ])
+
     preprocessor = ColumnTransformer([
         ("num", numeric_transformer, numeric_features),
-        ("cat", categorical_transformer, categorical_features),
-    ], remainder="drop")
+        ("cat", categorical_transformer, categorical_features)
+    ])
+
     return preprocessor
+
